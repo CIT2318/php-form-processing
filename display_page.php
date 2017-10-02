3f9fd8eeec9ea38d0072b79f42ee783efa40fb3a
mmantle-hud <m.e.mantle@hud.ac.uk>
2017-10-02 10:07:52 +0100

    simple solution

--- display_page.php
+++ display_page.php
@@ -1,3 +1,10 @@
+<?php
+$bg_colour = $_POST["bg_colour"];
+$txt_colour = $_POST["txt_colour"];
+$font = $_POST["font"];
+$heading_txt = $_POST["heading_txt"];
+$main_txt = $_POST["main_txt"];
+?>
 <!DOCTYPE HTML>
 <html>
 <head>
@@ -6,14 +13,16 @@
 </head>
 <style>
 body{
-    background-color:white;
-    color:black;
-    font-family:"Comic Sans";
+    background-color:<?php echo $bg_colour;?>;
+    color:<?php echo $txt_colour;?>;
+    font-family:<?php echo $font;?>;
 }
 </style>
 <body>
-<?php
-echo "User's form input should appear here"; 
+
+<?php 
+echo "<h1>".$heading_txt."</h1>";
+echo "<p>".$main_txt."</p>";
 ?>
 </body>
 </html>
